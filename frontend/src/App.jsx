8f1c8cf715de8bb886784fd37cd98feb2a1a1dee
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-28 23:03:36 +0530

    Added Footer inside App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="w-full ">
+    <div className="w-full bg-white ">
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
