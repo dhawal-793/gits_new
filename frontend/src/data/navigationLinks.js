da28b062eee4c3a2eb01232f5c22321d41a72040
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-29 15:26:51 +0530

    Created navigation data file.

---
+++ frontend/src/data/navigationLinks.js
@@ -0,0 +1,200 @@
+export const navLinks = [
+  {
+    title: "home",
+    href: "#",
+    subLinks: [
+      {
+        title: "About Us",
+        href: "#",
+        subLinks: [
+          {
+            title: "Welcome to GITS",
+            href: "/why-gits",
+          },
+          {
+            title: "Vision & Mission",
+            href: "/vision-mission",
+          },
+        ],
+      },
+      {
+        title: "Management Team",
+        href: "#",
+        subLinks: [
+          {
+            title: "Chairman's Message",
+            href: "/management-team/chairmans-message",
+          },
+          {
+            title: "Vice Chairman's Message",
+            href: "/management-team/vice-chairmans-message",
+          },
+          {
+            title: "Director's Message",
+            href: "/management-team/directors-message",
+          },
+          {
+            title: "Executive Director's Message",
+            href: "/management-team/executive-directors-message",
+          },
+          {
+            title: "Director's IQAC Message",
+            href: "/management-team/directors-iqac-message",
+          },
+          {
+            title: "Finance Controller's Message",
+            href: "/management-team/finance-controllers-message",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Statutory Info",
+    href: "#",
+    subLinks: [
+      {
+        title: "Governing Body",
+        href: "/governing-body",
+      },
+      {
+        title: "Collage Counsil",
+        href: "/collage-council",
+      },
+      {
+        title: "Commttees",
+        href: "#",
+        subLinks: [
+          {
+            title: "Anti Raging Committee",
+            href: "/anti-raging-committee",
+          },
+          {
+            title: "Internal Complaint Committee",
+            href: "/internal-complaint-committee",
+          },
+          {
+            title: "SC/ST Committee",
+            href: "/sc-st-committee",
+          },
+          {
+            title: "OBC Committee",
+            href: "/obc-committee",
+          },
+          {
+            title: "Library Committee",
+            href: "/library-committee",
+          },
+          {
+            title: "Women Empowerment Cell",
+            href: "/women-empowerment-cell",
+          },
+          {
+            title: "Industry Iinstitute Interaction Cell",
+            href: "/industry-institute-interaction-cell",
+          },
+        ],
+      },
+      {
+        title: "NAAC",
+        href: "#",
+        subLinks: [
+          {
+            title: "Curricular Aspects",
+            href: "/curricular-aspects",
+          },
+          {
+            title: "Teaching Learning And Evaluation",
+            href: "/teaching-learning-and-evaluation",
+          },
+          {
+            title: "Infrastructure And Learning Resources",
+            href: "/infrastructure-and-learning-resources",
+          },
+          {
+            title: "Student Support And Progression",
+            href: "/student-support-and-progression",
+          },
+          {
+            title: "Governance, LeaderShip And Management",
+            href: "/governance-leadership-and-management",
+          },
+          {
+            title: "Institutional Values And Best Practices",
+            href: "/institutional-values-and-best-practices",
+          },
+        ],
+      },
+      {
+        title: "IIC",
+        href: "/institutions-innovation-council",
+      },
+      {
+        title: "AICTE EAOs",
+        href: "/aicte-eaos",
+      },
+      {
+        title: "RTU Affiliation",
+        href: "/rtu-affiliation",
+      },
+      {
+        title: "Audit Reports",
+        href: "/audit-reports",
+      },
+      {
+        title: "RTI",
+        href: "/rti",
+      },
+      {
+        title: "NIRF",
+        href: "/nirf-reports",
+      },
+      {
+        title: "IQAC",
+        href: "/iqac",
+      },
+      {
+        title: "Feedback",
+        href: "/feedback",
+      },
+      {
+        title: "Code Practices & SWOT",
+        href: "/code-practices-swot",
+      },
+      {
+        title: "Institutional Best Practices",
+        href: "/institutional-best-practices",
+      },
+    ],
+  },
+  {
+    title: "Admission",
+    href: "#",
+    subLinks: [
+      {
+        title: "B.Tech",
+        href: "/admission/b-tech",
+      },
+      {
+        title: "M.Tech",
+        href: "/admission/m-tech",
+      },
+      {
+        title: "MBA",
+        href: "/admission/m-ba",
+      },
+      {
+        title: "MCA",
+        href: "/admission/mca",
+      },
+      {
+        title: "Hostel Facility",
+        href: "/hostel",
+      },
+    ],
+  },
+  {
+    title: "Departments",
+    href: "/departments",
+  },
+];
