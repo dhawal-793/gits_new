dd8e1898a5c3b973ec4b622cd89dac8a08edacb6
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-29 15:27:25 +0530

    Updated Navbar to use Navigation components along with navigation data.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import CustomButton from "./CustomButton";
+import NavItem from "./navigation/NavItem";
+
+import { navLinks } from "../data/navigationLinks";
 
 const Navbar = () => {
   const [isSticky, setIsSticky] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,11 +33,15 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${isSticky ? "fixed bg-white/20 backdrop-blur-sm top-0 transition-all ease-in duration-200 inset-x-0" : "static bg-white"} h-32`}
+      className={`${
+        isSticky
+          ? "fixed bg-white/40 backdrop-blur-sm top-0 transition-all ease-in duration-200 inset-x-0"
+          : "static bg-white"
+      } h-36`}
     >
       <div className="mx-auto max-w-screen-2xl  py-2 px-4">
         <div className="flex items-center justify-between w-full">
-          <div className="w-fit flex gap-2 items-center">
+          <Link to="/" className="w-fit flex gap-2 items-center">
             <img className="w-20 h-auto" src="/images/gits-logo.png" alt="" />
             <div className="w-80">
               <p className="font-semibold text-2xl">
@@ -37,30 +50,24 @@ const Navbar = () => {
             </div>
             {/* <img className="w-28 h-28" src="/images/nba-logo.png" alt="" />
           <img className="w-28 h-28" src="/images/rtu-logo.jpg" alt="" /> */}
-          </div>
+          </Link>
           <div className="flex items-center gap-5">
             <div className="fixed top-40 right-10 lg:static">
-            <CustomButton />
+              <CustomButton />
             </div>
-            <button className="">toggle</button>
+            <button
+              className="w-14 h-14 flex items-center justify-center text-2xl"
+              onClick={toggleNav}
+            >
+              <span className="">{isMenuOpen ? "X" : "#"}</span>
+            </button>
           </div>
         </div>
         <ul className="flex items-center justify-center gap-4 w-fit mx-auto">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/">Ranking</Link>
-          </li>
-          <li>
-            <Link to="/">Placements</Link>
-          </li>
-          <li>
-            <Link to="/">Program Offered</Link>
-          </li>
-          <li>
-            <Link to="/">Life at GITS</Link>
-          </li>
+          {navLinks.map((link, index) => (
+            <NavItem key={index} link={link} />
+          ))}
+         
         </ul>
       </div>
     </nav>
