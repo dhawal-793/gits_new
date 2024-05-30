de33b1030ea4fca5b28e1fc05fe6b4ac4b3562e0
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-30 14:06:24 +0530

    Updated some styles for CustomButton.

--- frontend/src/components/CustomButton.jsx
+++ frontend/src/components/CustomButton.jsx
@@ -5,9 +5,9 @@ const CustomButton = () => {
         href="https://www.gits.ac.in/apply-online/"
         target="_blank"
         rel="noopener noreferrer"
-        className="custom-btn relative inline-block text-white uppercase tracking-wider py-1 lg:py-2 px-5 lg:px-10 font-semibold textsm lg:text-lg rounded-lg bg-[#1760A6] border-2 border-[#1760A6] overflow-hidden group"
+        className="custom-btn relative inline-block text-white uppercase tracking-wider py-1 lg:py-2 px-5 lg:px-10 font-semibold textsm lg:text-lg rounded-lg bg-secondary border-2 border-secondary overflow-hidden group"
       >
-        <span className=" relative z-[5] inset-0 group-hover:text-transparent group-hover:bg-gradient-to-r from-[#1760a6] to-[#1760a6] group-hover:bg-clip-text group-hover:transition-colors duration-500 ease-out">Apply Now</span>
+        <span className=" relative z-[5] inset-0 group-hover:text-transparent group-hover:bg-gradient-to-r from-secondary to-secondary group-hover:bg-clip-text group-hover:transition-colors duration-500 ease-out">Apply Now</span>
         <span className="absolute inset-0 bg-white transform scale-x-0 transition-transform duration-500 ease-out origin-left z-0 group-hover:scale-x-100"></span>
       </a>
     </div>
