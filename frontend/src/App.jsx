f417ac243cd49fe02ffc2d49b326a11fa7312a05
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-30 14:40:22 +0530

    Updated App to use AllRoutes, added Navtoggle Button, updated some styles.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,84 +1,40 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import ExtraPage from "./pages/ExtraPage";
-import Departments from "./pages/Departments";
-import Blog from "./pages/Blog";
-import Contact from "./pages/Contact";
-import Placements from "./pages/Placements";
-import YearPlacements from "./pages/YearPlacements";
-import GITSAlumniForum from "./pages/GITSAlumniForum";
-import ResearchInnovation from "./pages/ResearchInnovation";
-import FundedResearch from "./pages/FundedResearch";
-import FacultyPublications from "./pages/FacultyPublications";
-import COEIndustrialAutomation from "./pages/COEIndustrialAutomation";
-import Hostel from "./pages/Hostel";
-import CourseAdmission from "./pages/CourseAdmission";
-import WhyGits from "./pages/WhyGits";
-import VisionAndMission from "./pages/VisionAndMission";
-import ManagementTeamsMessage from "./pages/ManagementTeamsMessage";
-import Committee from "./pages/Committee";
+import AllRoutes from "./AllRoutes";
 
 function App() {
+  const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleNav = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
   return (
     <div className="w-full bg-white relative">
-      <div className={`w-full bg-white ${isMenuOpen ? "fixed inset-0 pr-2" : "static"} `}>
-      <Navbar isMenuOpen={isMenuOpen} toggleNav={toggleNav} />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/contact-us" element={<Contact />} />
-          <Route exact path="/blog" element={<Blog />} />
-          <Route exact path="/departments" element={<Departments />} />
-          <Route exact path="/placements" element={<Placements />} />
-          <Route exact path="/placements/:year" element={<YearPlacements />} />
-          <Route
-            exact
-            path="/gits-alumni-forum"
-            element={<GITSAlumniForum />}
+      <div
+        className={`w-full bg-white ${
+          isMenuOpen ? "fixed inset-0" : "static"
+        } `}
+      >
+        <Navbar isMenuOpen={isMenuOpen}/>
+        <button
+          className="fixed top-4 right-4  w-14 h-14 flex items-center justify-center z-90 text-primary lg:hidden"
+          onClick={toggleNav}
+        >
+          <span
+            className={`text-3xl fa-solid ${
+              isMenuOpen ? "fa-xmark -rotate-180" : "fa-bars rotate-0"
+            } transition-all duration-200`}
           />
-          <Route
-            exact
-            path="/research-innovation"
-            element={<ResearchInnovation />}
-          />
-          <Route exact path="/funded-research" element={<FundedResearch />} />
-          <Route
-            exact
-            path="/faculty-publications"
-            element={<FacultyPublications />}
-          />
-          <Route
-            exact
-            path="/coe-industrial-automation"
-            element={<COEIndustrialAutomation />}
-          />
-          <Route exact path="/hostel" element={<Hostel />} />
-          <Route
-            exact
-            path="/admission/:course"
-            element={<CourseAdmission />}
-          />
-          <Route exact path="/why-gits" element={<WhyGits />} />
-          <Route exact path="/vision-mission" element={<VisionAndMission />} />
-          <Route
-            exact
-            path="/management-team/:management"
-            element={<ManagementTeamsMessage />}
-          />
-          <Route exact path="/committee/:committee" element={<Committee />} />
-
-          <Route exact path="/:id" element={<ExtraPage />} />
-        </Routes>
+        </button>
+        <AllRoutes />
         <Footer />
       </div>
     </div>
