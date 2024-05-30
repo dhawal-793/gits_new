7d3b143e801bade3ed2c0bdf2bfe5e75c59489e4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-30 15:15:03 +0530

    Updated SideNav to use navigateTo method instead of Link component for navigation.

--- frontend/src/components/navigation/SideNav.jsx
+++ frontend/src/components/navigation/SideNav.jsx
@@ -1,9 +1,8 @@
 import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
 
 import { navLinks } from "../../data/navigationLinks";
 
-const SideNav = () => {
+const SideNav = ({ isMenuOpen, navigateTo }) => {
   const [currentOpenLink, setCurrentOpenLink] = useState("");
 
   const toggleNavigationLinks = (title) => {
@@ -13,69 +12,80 @@ const SideNav = () => {
   };
 
   return (
-    <div className="flex h-screen w-full flex-col items-end justify-start pt-20">
-      <ul className="space-y-4 py-6 overflow-y-scroll w-full h-full">
-        {navLinks.map((link, index) => {
-          return (
-            <li
-              key={index}
-              className="relative w-full text-white"
-              onMouseEnter={() => toggleNavigationLinks(link.title)}
-              onMouseLeave={() => toggleNavigationLinks("")}
-            >
-              {link.subLinks ? (
-                <>
-                  <button
-                    onClick={() => toggleNavigationLinks(link.title)}
-                    className="w-full text-right cursor-pointer font-semibold hover:text-primary transition-colors duration-200 px-6"
-                  >
-                    {link.title}
-                  </button>
-                  <div
-                    className={`bg-white/20 backdrop-blur-lg mt-2 w-full text-right shadow-lg border-t-4 border-primary pt-2 mr-6 transition-all duration-200  ${
-                      currentOpenLink == link.title
-                        ? "h-full relative  opacity-100  duration-300 ease-in translate-y-0"
-                        : "h-0 absolute opacity-0 pointer-events-none -translate-y-5"
-                    }`}
-                  >
-                    <ul className="">
-                      {link.subLinks.map((subLink, index) => (
-                        <SubNavItem key={index} subLink={subLink} />
-                      ))}
-                    </ul>
-                  </div>
-                </>
-              ) : (
-                <>
-                  {link.external ? (
-                    <a
-                      href={link.href}
-                      target="_blank"
-                      className="block w-full text-right cursor-pointer px-6 font-semibold hover:text-primary transition-colors duration-200"
+    <nav
+      className={`fixed inset-x-0 top-0 h-full max-h-screen  w-full bg-black/90 backdrop-blur-sm transition-all duration-200 ${
+        isMenuOpen ? "translate-x-0" : "translate-x-full"
+      } z-[60]  lg:hidden`}
+    >
+      <div className="flex h-screen w-full flex-col items-end justify-start pt-20">
+        <ul className="space-y-4 py-6 overflow-y-scroll w-full h-full">
+          {navLinks.map((link, index) => {
+            return (
+              <li
+                key={index}
+                className="relative w-full text-white"
+                onMouseEnter={() => toggleNavigationLinks(link.title)}
+                onMouseLeave={() => toggleNavigationLinks("")}
+              >
+                {link.subLinks ? (
+                  <>
+                    <button
+                      onClick={() => toggleNavigationLinks(link.title)}
+                      className="w-full text-right cursor-pointer font-semibold hover:text-primary transition-colors duration-200 px-6"
                     >
                       {link.title}
-                    </a>
-                  ) : (
-                    <Link
-                      to={link.href}
-                      className="block w-full text-right cursor-pointer px-6 font-semibold hover:text-primary transition-colors duration-200"
+                    </button>
+                    <div
+                      className={`bg-white/20 backdrop-blur-lg mt-2 w-full text-right shadow-lg border-t-4 border-primary pt-2 mr-6 transition-all duration-200  ${
+                        currentOpenLink == link.title
+                          ? "h-full relative  opacity-100  duration-300 ease-in translate-y-0"
+                          : "h-0 absolute opacity-0 pointer-events-none -translate-y-5"
+                      }`}
                     >
-                      {link.title}
-                    </Link>
-                  )}
-                </>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                      <ul className="">
+                        {link.subLinks.map((subLink, index) => (
+                          <SubNavItem
+                            key={index}
+                            subLink={subLink}
+                            navigateTo={navigateTo}
+                          />
+                        ))}
+                      </ul>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {link.external ? (
+                      <a
+                        href={link.href}
+                        target="_blank"
+                        className="block w-full text-right cursor-pointer px-6 font-semibold hover:text-primary transition-colors duration-200"
+                      >
+                        {link.title}
+                      </a>
+                    ) : (
+                      <button
+                        onClick={() => navigateTo(link.href)}
+                        // href={link.href}
+                        className="block w-full text-right cursor-pointer px-6 font-semibold hover:text-primary transition-colors duration-200"
+                      >
+                        {link.title}
+                      </button>
+                    )}
+                  </>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
 export default SideNav;
 
-const SubNavItem = ({ subLink }) => {
+const SubNavItem = ({ subLink, navigateTo }) => {
   const [isOpen, setIsOpen] = useState(false);
   const timeoutRef = useRef(null);
 
@@ -128,7 +138,9 @@ const SubNavItem = ({ subLink }) => {
                       {nestedSubLink.title}
                     </a>
                   ) : (
-                    <Link to={nestedSubLink.href}>{nestedSubLink.title}</Link>
+                    <button onClick={()=>navigateTo(nestedSubLink.href)}>
+                      {nestedSubLink.title}
+                    </button>
                   )}
                 </li>
               ))}
@@ -146,12 +158,12 @@ const SubNavItem = ({ subLink }) => {
               {subLink.title}
             </a>
           ) : (
-            <Link
-              to={subLink.href}
-              className="block px-4 hover:text-primary text-sm py-2 font-semibold"
+            <button
+              onClick={() => navigateTo(subLink.href)}
+              className="px-4 hover:text-primary text-sm py-2 font-semibold"
             >
               {subLink.title}
-            </Link>
+            </button>
           )}
         </>
       )}
