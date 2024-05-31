7fd567945bead2624c40e248100a4c1597491d9b
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-31 20:27:54 +0530

    Created SocialLinks component to display all the sociallink using socialLinks data, updated styles.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import Container from "./ui/Container";
 
+import { socialLinks } from "../data/socialLinks";
+
+
 const linksData = [
   {
     title: "Useful Links",
@@ -17,9 +20,11 @@ const linksData = [
   },
 ];
 
+
+
 const Footer = () => {
   return (
-    <footer className="bg-[#273044] w-full text-white">
+    <footer className="bg-tertiary w-full text-white py-4">
       <Container>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-7 gap-x-8 py-10 ">
           {linksData.map((linksData, idx) => (
@@ -47,15 +52,13 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <div>
-          <p>Copyright © 2021. GITS. All Rights Reserved</p>
-          <p>Visitors: 4013800</p>
-          <div className="flex items-center gap-6 pt-4">
-            <i className="cursor-pointer fa-brands fa-facebook-f" />
-            <i className="cursor-pointer fa-brands fa-twitter" />
-            <i className="cursor-pointer fa-brands fa-instagram" />
-            <i className="cursor-pointer fa-brands fa-linkedin-in" />
-            <i className="cursor-pointer fa-brands fa-youtube" />
+        <div className="lg:flex items-center justify-between w-full">
+          <p className="lg:text-sm">Copyright © 2021. GITS. All Rights Reserved</p>
+          <p className="lg:text-xs">Visitors: 4013800</p>
+          <div className="flex items-center gap-3 pt-4 lg:pt-0 lg:pr-5">
+            {socialLinks.map((link) => (
+              <SocialLinks key={link.href} {...link} />
+            ))}
           </div>
         </div>
       </Container>
@@ -73,13 +76,29 @@ const FooterLinkGroup = ({ linkData }) => {
         {linkData.links.map((link) => (
           <li
             key={link}
-            className="border-b border-gray-500 pb-1 w-full flex items-center gap-3 cursor-pointer"
+            className="border-b border-gray-500 pb-1 w-full flex items-center gap-3 cursor-pointer hover:text-primary"
           >
-            <i className="fa-solid fa-chevron-right text-yellow-600 text-xs" />
+            <i className="fa-solid fa-chevron-right text-primary text-xs" />
             <span>{link}</span>
           </li>
         ))}
       </ul>
     </div>
+  );
+};
+
+const SocialLinks = ({ href, color, icon }) => {
+  return (
+    <li className="list-none ">
+      <a
+        href={href}
+        target="_blank"
+        className={`size-9 rounded-full bg-tertiary ${color} group transition-all duration-300 flex items-center justify-center shadow-md shadow-white/20`}
+      >
+        <i
+          className={`cursor-pointer fa-brands ${icon} text-white group-hover:rotate-[360deg] transition-all duration-300`}
+        />
+      </a>
+    </li>
   );
 };
