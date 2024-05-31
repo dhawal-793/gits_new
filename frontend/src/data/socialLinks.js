58c833625139b14dcb18ac636df342d334cabca5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-31 20:26:05 +0530

    Created a socialLinks file for all the socialLinks..

---
+++ frontend/src/data/socialLinks.js
@@ -0,0 +1,27 @@
+export const socialLinks = [
+  {
+    icon: "fa-facebook-f",
+    href: "https://www.facebook.com/gitsudr",
+    color: "hover:bg-blue-500",
+  },
+  {
+    icon: "fa-twitter",
+    href: "https://x.com/gitsudr",
+    color: "hover:bg-sky-500",
+  },
+  {
+    icon: "fa-instagram",
+    href: "https://www.instagram.com/gitsudr",
+    color: "hover:bg-pink-500",
+  },
+  {
+    icon: "fa-linkedin-in",
+    href: "https://www.linkedin.com/school/geetanjali-institute-of-technical-studies-udaipur",
+    color: "hover:bg-blue-500",
+  },
+  {
+    icon: "fa-youtube",
+    href: "https://www.youtube.com/channel/UCPHu7GshMSC8zb77I7SKK0w/videos",
+    color: "hover:bg-red-500",
+  },
+];
