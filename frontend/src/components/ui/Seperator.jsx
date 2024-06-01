b09da73a757160a55534122b5e3b1624b21c0559
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-01 18:42:41 +0530

    Created a Seperator component.

---
+++ frontend/src/components/ui/Seperator.jsx
@@ -0,0 +1,10 @@
+const Seperator = () => {
+  return (
+    <div className="w-full flex items-center gap-5 my-8 md:my-14">
+      <div className="w-12 bg-primary h-[3px]" />
+      <div className="w-full bg-tertiary/20 h-[1px]" />
+    </div>
+  );
+};
+
+export default Seperator;
