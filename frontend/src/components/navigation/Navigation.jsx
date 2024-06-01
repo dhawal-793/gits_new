4af51605b4aee2cd0ebab0e65983ceb7070cefa1
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-01 17:15:06 +0530

    Created a Navigation component to wrap all the Navigation logic and components.

---
+++ frontend/src/components/navigation/Navigation.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import Navbar from "./Navbar";
+import SideNav from "./SideNav";
+
+const Navigation = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigateTo = (url) => {
+    console.log(url);
+    navigate(url);
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <>
+      <Navbar />
+      <SideNav isMenuOpen={isMenuOpen} navigateTo={navigateTo} />
+      <button
+        className={`fixed top-0.5 translate-y-1/2 right-4 z-[90] text-primary lg:hidden text-3xl w-fit flex items-center justify-center`}
+        onClick={toggleNav}
+      >
+        <span
+          className={`fa-solid ${
+            isMenuOpen ? "fa-xmark -rotate-180" : "fa-bars rotate-0"
+          } transition-all duration-200`}
+        ></span>
+      </button>
+    </>
+  );
+};
+
+export default Navigation;
