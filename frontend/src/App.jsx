9cce14368ee9905387f8a02496a2de460f532f68
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-01 17:15:32 +0530

    Updated App to use Navigation component and code cleanup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,13 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import SideNav from "./components/navigation/SideNav";
+import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/Footer";
 import BackToTopButton from "./components/BackToTopButton";
 
 import AllRoutes from "./AllRoutes";
 
 function App() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const navigateTo = (url) => {
-    console.log(url);
-    navigate(url);
-    setIsMenuOpen(false);
-  };
-
-  useEffect(() => {
-    setIsMenuOpen(false);
-  }, [location.pathname]);
   return (
     <div className="w-full bg-white relative">
-      <Navbar />
-      <SideNav isMenuOpen={isMenuOpen} navigateTo={navigateTo} />
-
-      <button
-        className={`fixed top-0.5 translate-y-1/2 right-4 z-[90] text-primary lg:hidden text-3xl w-fit flex items-center justify-center`}
-        onClick={toggleNav}
-      >
-        <span
-          className={`fa-solid ${
-            isMenuOpen ? "fa-xmark -rotate-180" : "fa-bars rotate-0"
-          } transition-all duration-200`}
-        ></span>
-      </button>
+      <Navigation />
       <AllRoutes />
       <BackToTopButton />
       <Footer />
