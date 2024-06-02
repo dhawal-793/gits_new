56d21e83ac52411771541c3b8df762669fe26a7d
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-02 14:16:04 +0530

    Created a Title UI Component to display the Title of the Page.

---
+++ frontend/src/components/ui/Title.jsx
@@ -0,0 +1,12 @@
+
+const Title = ({title}) => {
+  return (
+    <div className="w-full bg-secondary py-8 ">
+        <h1 className="text-center text-white text-4xl font-medium uppercase">
+          {title}
+        </h1>
+      </div>
+  )
+}
+
+export default Title
