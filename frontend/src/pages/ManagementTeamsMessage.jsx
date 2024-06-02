5263d67d332a7a46fbcf8c8ad7a9501307ad67e7
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-02 18:06:24 +0530

    Updated the ManagementTeamsMessage page to fetch data from server.

--- frontend/src/pages/ManagementTeamsMessage.jsx
+++ frontend/src/pages/ManagementTeamsMessage.jsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-import { getMessage } from "../data/managements-message";
+import axios from "axios";
 
 import NotFound from "../components/ui/NotFound";
 import Title from "../components/ui/Title";
 import Heading from "../components/ui/Heading";
 import Container from "../components/ui/Container";
 
+
+const BASE_URL = import.meta.env.VITE_HOST
+
 const ManagementTeamsMessage = () => {
   const { management } = useParams();
-  const title = management.split("-").join("_");
-  const data = getMessage(title);
+  const [data, setData] = useState(null);
 
-  if (data == "not found") {
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/api/managements-message/${management}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log('Error',error.message);
+        if (error.response.status) setData("404");
+      });
+  }, [management]);
+
+  if (data == "404") {
     return <NotFound />;
   }
+  if (!data) {
+  return <div>Something Went wrong</div>
+}
 
   return (
     <div>
