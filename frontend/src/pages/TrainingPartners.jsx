7a6b24ca91cf739ad549b9449fc8586666574bac
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-03 14:31:44 +0530

    Created Training Partners page and fetched data from api.

---
+++ frontend/src/pages/TrainingPartners.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import Container from "../components/ui/Container";
+import Heading from "../components/ui/Heading";
+import Title from "../components/ui/Title";
+import NotFound from "../components/ui/NotFound";
+
+const BASE_URL = import.meta.env.VITE_HOST;
+
+const TrainingPartners = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchPartners = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${BASE_URL}/api/partners/training-partners`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error.message);
+        if (error.response.status == 404) {
+          setData("404");
+        } else {
+          setError(error.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPartners();
+  }, []);
+
+  useEffect(() => {}, [data]);
+
+  if (data == "404") {
+    return <NotFound />;
+  }
+
+  if (loading) {
+    return <div className="text-center py-16 text-sm">Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div className="text-center py-16 text-red-500 text-lg">
+        Internal Server Error
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {data && (
+        <>
+          <Title title="TRAINING PARTNERS" />
+          <Container>
+            <div className="py-6">
+              {data.map(({ name, image, description }) => (
+                <div key={name} className="py-3">
+                  <Heading>
+                    <span className="uppercase">{name}</span>
+                  </Heading>
+                  <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 lg:items-start -mt-3">
+                    <div className=" h-40 mx-auto relative flex items-center justify-center p-3 border-4 border-primary">
+                      <img
+                        className="lg:w-80 lg:max-w-[20rem] h-32"
+                        src={image}
+                        alt={name}
+                      />
+                    </div>
+                    <div className="text-sm text-justify space-y-3">
+                      {description.map((para, idx) => (
+                        <p key={idx}>{para}</p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Container>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TrainingPartners;
