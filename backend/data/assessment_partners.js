db1e1e409aa3769df4e896ab70b37d87bfe9c78c
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-03 14:15:15 +0530

    Added data for training and assessment partners.

---
+++ backend/data/assessment_partners.js
@@ -0,0 +1,39 @@
+export const assessment_partners = [
+  {
+    name: "AMCAT",
+    image: "images/assessment_partners/AMCAT.png",
+    description: [
+      "Aspiring Minds Computer Adaptive is  a computer adaptive test which measures job applicants on critical areas like communication skills, logical reasoning, quantitative skills and job specific domain skills thus helping recruiters identify the suitability of a candidate.",
+      "GITS have tie up with AMCAT for pre final year & final year B.Tech students.",
+    ],
+  },
+  {
+    name: "COCUBES",
+    image: "images/assessment_partners/Cocubes.jpg",
+    description: [
+      "CoCubes.com is India's largest assessment and campus hiring platform. They work with 450+ corporate across fresher’s and lateral’s, 2600+ colleges, and 1 million+ students through their platform running 500,000 assessments each year in 250+ cities. Thus everyday helping our corporate get greater control and transparency, helping our institutional clients measure and improve employability and helping students move ahead on their career path.",
+    ],
+  },
+  {
+    name: "MONSTER COLLEGE",
+    image: "images/assessment_partners/Monster.jpg",
+    description: [
+      "Monster College™, a customized platform aiming to aid educational institutes with placement support and employ ability enhancing services.",
+      "They endeavor to streamline the hiring efforts of companies; help them find right candidates and pave ways for recent graduates to explore new career opportunities. And they do this by bringing three entities i.e. Colleges, Students and Companies together.",
+    ],
+  },
+  {
+    name: "E-LITMUS",
+    image: "images/assessment_partners/Elitmus.jpg",
+    description: [
+      "E-Litmus is a first-of-its-kind service in the world. Since it was launched in 2005, eLitmus has been redefining how fresher and entry-level recruitment is done in India.An extensive range of online tools that are provided by E-litmus, help recruiters to easily screen and select candidates based on this and dozens of other parameters.",
+    ],
+  },
+  {
+    name: "METTL: Online Skill Assessment Platform",
+    image: "images/assessment_partners/Mettl.jpg",
+    description: [
+      "Mettl, India's leading online skill assessment platform, has introduced an innovative new tool to its successful lineup. The company’s new case study simulator aims to bring case study methodology to online assessments. Testers can simply log on to the Mettl platform and participate in the treasure hunt, better described as the gamification of a business case problem, upon invitation from a participating company or institution.",
+    ],
+  },
+];
---
+++ backend/data/training_partners.js
@@ -0,0 +1,90 @@
+export const training_partners = [
+  {
+    name: "Tata CMC Limited",
+    image: "/images/training_partners/tata-cmc.jpg",
+    description: [
+      "CMC Limited is a leading IT solutions company and a subsidiary of Tata Consultancy Services Limited (TCS Ltd), one of the world's leading information technology consulting, services and business process outsourcing organisations. They are a part of the Tata group, India's best-known business conglomerate. Today, CMC Limited, an ISO 9001:2000, certified and CMMI Level V accredited organisation, is positioned as a premier IT solutions provider in the fast growing and competitive IT market.",
+      "CMC Ltd., a Tata entrerprise, has been at the forefront of the IT revolution that has catapulted India to its exalted status in the global IT arena. It is leading IT Solutions Company, a subsidiary of Tata Consultancy Services Ltd., with an enviable record of building IT solutions for massive and complex projects across the value chain infrastructure, applications and business processes. Its capabilities span the entire IT spectrum: IT; architecture; hardware; software;network consulting.",
+    ],
+  },
+  {
+    name: "TCI",
+    image: "/images/training_partners/tcil.png",
+    description: [
+      "Transport Corporation of India (TCI), with revenues of over Rs.2900 crore, is India's leading integrated multimodal logistics and supply chain solutions provider. As “Leaders in Logistics”, TCI continuously strives to better existing systems, processes and productivity. With expertise spanning over six decades and infrastructure comprising an extensive network of 1400+ company owned offices, 11million sq.ft of warehousing space, a strong team of 6000+ trained employees and a strong foundation, TCI has expanded boundaries to offers seamless multimodal transportation solutions in both Indian and International markets.",
+    ],
+  },
+  {
+    name: "NIYO",
+    image: "/images/training_partners/niyo.jpg",
+    description: [
+      "NiYO Solutions Inc., an emerging fintech startup for salaried employees, has raised $13.2 million (85 crore) in Series A funding to drive the continued expansion of its payroll and benefits platform in India. The investment was made by Social Capital, JS Capital LLc (the family office of Jonathan Soros), and Horizons Ventures with participation by existing investor, Prime Venture Partners. In total, the company has raised more than $14 million in venture capital. Adding on to its continued growth and popularity among businesses across India, NiYO aims to serve 5000 organizations and over 1 million salaried employees by the end of 2018.",
+    ],
+  },
+  {
+    name: "Amul",
+    image: "/images/training_partners/amul.png",
+    description: [
+      "Amul is an Indian dairy cooperative , based at Anand in the state of Gujarat, India.Formed in 1948, it is a brand managed by a cooperative body, the Gujarat Co-operative Milk Marketing Federation Ltd. (GCMMF), which today is jointly owned by 3.6 million milk producers in Gujarat. Amul is actually the abbreviated form of Anand Milk Union Limited. The white revolution was spearheaded by Tribhuvandas Patel under the guidance of Sardar Patel and Verghese Kurien. As a result, Kaira District Milk Union Limited was born in 1946. Tribhuvan das became the founding chairman of the organization which he led till his last day of his life. He hired Dr. Kurien three years after the white revolution. He convinced Dr.Kurien to stay and help with the mission.",
+    ],
+  },
+  {
+    name: "Oracle",
+    image: "/images/training_partners/oracle.png",
+    description: [
+      "The Oracle Academy is a cutting-edge program that provides global education institutions with industry-leading software, curriculum, support, and certification resources that faculty can integrate into their classrooms. As a result, students gain hands-on experience with the latest technologies and develop skills that help them excel in the 21st century workplace.",
+      "The courseware of oracle academy is designed to be delivered as part of an academic program of study, has educational learning objectives, and is modular, enabling teachers and faculty members to incorporate entire Oracle Academy courses, individual lessons or topics into their existing classes and programs of study.",
+    ],
+  },
+  {
+    name: "FACE",
+    image: "/images/training_partners/face.jpg",
+    description: [
+      "FACE is one of India's Largest Higher Education focused skill development company, catering to students pursuing higher education in colleges/universities as well as entry level recruits in organisations across various industry sectors. FACE has evolved a scalable business model which is one of its kind and has been able to create phenomenal success stories for its clients through the same. In a span of 6 years since inception FACE has trained more than 12,00,000+ candidates across 1000+ educational institutions, corporate organisations and has emerged as the market leader in the placement training industry.",
+    ],
+  },
+  {
+    name: "Texas Instruments",
+    image: "/images/training_partners/texas.png",
+    description: [
+      "TI is a global semiconductor design and manufacturing company. Innovate with 100,000+ analog ICs and embedded processors, along with software, tools and the industry's largest sales/support staff.The extensive training program offers hundreds of online training courses, webinars and in-person seminars to help you design with TI products, development tools, software and applications.",
+      "A MOU has been signed with EDGATE Technologies Pvt. Ltd. to set up an Analog lab of Texas Instruments kits at GITS on 1 October, 2014, through which TI had donated 6 ASLK kits for students of GITS.",
+    ],
+  },
+  {
+    name: "CDAC",
+    image: "/images/training_partners/cdac.jpg",
+    description: [
+      "Centre for Development of Advanced Computing (C-DAC) is the premier R&D organization of the Department of Electronics and Information Technology (DeitY), Ministry of Communications & Information Technology (MCIT) for carrying out R&D in IT, Electronics and associated areas.  Different areas of C-DAC, had originated at different times, many of which came out as a result of identification of opportunities.",
+      "C-DAC's Education and Training programmes are aimed at creating skilled manpower in the country by providing quality training programmes in the field of Electronics and ICT.",
+    ],
+  },
+  {
+    name: "Advaiya",
+    image: "/images/training_partners/advaiya.png",
+    description: [
+      "Advaiya comprises of a team of professional services consultants that advances an organization's business, technology, initiatives and marketing goals. It helps companies build a vision and craft their story with services like strategic consulting, technical & marketing content, training and evangelism, software development, creative expertise and staffing. Derived from the Sanskrit word describing 'intrinsic oneness', Advaiya was established in Udaipur based on the conviction of removing the apparent dichotomy of business and technology in 2005. Within less than a decade, it has rapidly evolved to extend the breadth of its experience and depth of its capabilities for large, niche players as well as innovative startups.",
+    ],
+  },
+  {
+    name: "Adani Vilmar",
+    image: "/images/training_partners/adani-wilmar.jpg",
+    description: [
+      "Adani Wilmar Limited (AWL) is a joint venture incorporated in January 1999 between Adani Group- The leaders in private infrastructure and Wilmar International Limited - Singapore, Asia's leading Agri business group. The Adani group was created with a vision of 'Nation Building' by developing assets of national economic significance. Wilmar International Limited - Singapore, Asia's leading Agri-business group & its business activities include oil palm cultivation, oilseed crushing, edible oil refining, sugar milling and refining, specialty fat,oleochemical, biodiesel and fertilizer manufacturing and grain processing. It has over 450 manufacturing plants and an extensive distribution network covering China, India, Indonesia and some 50 other countries.",
+    ],
+  },
+  {
+    name: "Agarwal",
+    image: "/images/training_partners/agarwal.jpg",
+    description: [
+      "Agarwal Packers and Movers Ltd., one of the largest Logistics companies in India, evolved from a small scale business of Agarwal Household Carrier established in 1987 and functioned particularly for the shifting of household goods. Incepted by vision and hard efforts of Shri Ramesh Agarwal and Shri Rajender Agarwal, the company moved ahead towards the path of growth. This evolution takes place on the solid grounds of expansion in terms of proficient and customer-satisfactory services. We have followed the principles of growth ever since our inception.",
+    ],
+  },
+  {
+    name: "Pacific Industries ltd. India",
+    image: "/images/training_partners/pacific.jpg",
+    description: [
+      "Pacific Industries Ltd was established in the year 1989 as a 100% export oriented unit by eminent industrialist Mr. J.P. Agrawal and today we are one of the leading quarry owners and processors of natural stones. With products like slabs, tiles, cut to size, we are enjoying our presence in over 40 countries. With our two processing units each at Bangalore and Udaipur we make sure that our customers have access to all the Indian colours at attractive prices.",
+    ],
+  },
+];
