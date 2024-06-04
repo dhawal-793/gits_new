3e20603c550d569e6a3dbc320a5bb94b7f1bf16a
Somya Panchal <somyapancom@gmail>
2024-06-04 17:27:43 +0530

    added carosel in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import SimpleCarousel from "../components/home-components/SimpleCarosel";
 import SectionOne from "../components/home-components/SectionOne";
 
 const Home = () => {
 
   return (
     <>
+    <SimpleCarousel/>
       <SectionOne />
     </>
   );
