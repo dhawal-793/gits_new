00b41187afa96d50193d557780f819d51532ef5f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-04 19:41:55 +0530

    Updated Carousel settings.

--- frontend/src/components/home-components/Carousel.jsx
+++ frontend/src/components/home-components/Carousel.jsx
@@ -13,27 +13,27 @@ const images = [
   "images/carousel_images/m_banner.jpeg",
 ];
 
-const NextArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer z-10"
-      onClick={onClick}
-    >
-      <i className="fa-solid fa-chevron-right text-3xl sm:xl text-primary text-opacity-50 hover:text-opacity-80"></i>
-    </div>
-  );
-};
+// const NextArrow = ({ onClick }) => {
+//   return (
+//     <div
+//       className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer z-10"
+//       onClick={onClick}
+//     >
+//       <i className="fa-solid fa-chevron-right text-3xl sm:xl text-primary text-opacity-50 hover:text-opacity-80"></i>
+//     </div>
+//   );
+// };
 
-const PrevArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute top-1/2 left-4 transform -translate-y-1/2 cursor-pointer z-10"
-      onClick={onClick}
-    >
-      <i className="fa-solid fa-chevron-left text-3xl sm:xl text-primary text-opacity-50 hover:text-opacity-80"></i>
-    </div>
-  );
-};
+// const PrevArrow = ({ onClick }) => {
+//   return (
+//     <div
+//       className="absolute top-1/2 left-4 transform -translate-y-1/2 cursor-pointer z-10"
+//       onClick={onClick}
+//     >
+//       <i className="fa-solid fa-chevron-left text-3xl sm:xl text-primary text-opacity-50 hover:text-opacity-80"></i>
+//     </div>
+//   );
+// };
 
 const Carousel = () => {
   const settings = {
@@ -43,16 +43,17 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
-    // adaptiveHeight:true,
     fade: true,
+    autoplaySpeed: 5000,
+    // swipeToSlide: true,
+    // adaptiveHeight:true,
     // pauseOnHover: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
   };
 
   return (
-    <div className="relative w-full aspect-video">
+    <div className="relative w-full aspect-video overflow-hidden">
       <Slider {...settings}>
         {images.map((src, index) => (
           <div key={index}>
