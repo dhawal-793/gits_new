3234686fb385772f1033fb90ddbfb098b20d5c87
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-05 19:41:27 +0530

    Created CurricularAspect page.

---
+++ frontend/src/pages/CurricularAspect.jsx
@@ -0,0 +1,20 @@
+import Container from '../components/ui/Container'
+import Title from '../components/ui/Title'
+
+const CurricularAspect = () => {
+  return (
+    <>
+      <Title title="CURRICULAR ASPECTS" />
+      <Container>
+        <div className="py-6">
+          <>
+            {/* {loading && <div className="text-center text-sm">Loading...</div>}
+            {data && <Partners data={data} />} */}
+          </>
+        </div>
+      </Container>
+    </>
+  )
+}
+
+export default CurricularAspect
