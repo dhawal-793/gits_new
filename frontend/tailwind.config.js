a62e66271fbe390b167cef62b785a72d306068d0
Somya Panchal <somyapancom@gmail>
2024-06-06 17:38:43 +0530

    created custom css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,9 +10,33 @@ export default {
         'primary':'#FFB700',
         'secondary':'#1760A6',
         'tertiary':'#273044',
+        
+      },
+      backgroundImage: {
+        'custom-background': "url('images/bg.webp')",
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0))',
+      },
+      // backgroundImage: {
+      //   'yellow-gradient': 'linear-gradient(180deg, rgba(255, 255, 0, 0.5), rgba(255, 255, 0, 0))',
+      // }
+      backgroundImage: {
+        'orange-gradient': 'linear-gradient(180deg, rgba(255, 165, 0, 0.5), rgba(255, 165, 0, 0))',
       }
     },
   },
-  plugins: [],
+  
+    plugins: [
+      function ({ addUtilities }) {
+        const newUtilities = {
+          '.tap-highlight-transparent': {
+            '-webkit-tap-highlight-color': 'transparent',
+          },
+        };
+  
+        addUtilities(newUtilities, ['responsive', 'hover']);
+      },
+    ],
 }
 
