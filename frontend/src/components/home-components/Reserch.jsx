ffe37d8e8577b0d93895b5b41f5003c0a58ea887
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-07 20:33:20 +0530

    Updated structure and styles for Research component.

--- frontend/src/components/home-components/Reserch.jsx
+++ frontend/src/components/home-components/Reserch.jsx
@@ -1,41 +1,40 @@
-// src/components/HomePage.js
-
-import React from 'react';
-
+import Container from "../ui/Container";
 import CustomButton from "../CustomButton";
+
 const Reserch = () => {
   return (
-    <div className="bg-white  ">
-      <div className="container mx-auto px-2 sm:px-4 pt-10 sm:py-16">
-        <div className="flex flex-col md:flex-row items-center md:justify-between">
-          
-          <div className="md:w-1/2 sm:pl-14 lg:pl-14 align-text-top md:text-left">
-            <h1 className="text-2xl  text-primary ">Reserch at GITS</h1>
-            <h1 className="text-3xl lg:text-5xl sm:text-5xl font-semibold text-[#231f20] pt-8  ">EXPLORING THE</h1>
-            <h1 className="text-3xl sm:text-5xl lg:text-5xl font-semibold text-[#231f20] pt-4 pb-10 ">UNEXPLORED</h1>
-            <p>Rapid growth in scientific knowledge is an indication of quest for innovations which has an impact on economic and societal development. Science, technology and innovation is often initiated in the College environment and it is also the mission of Geetanjali Institute Of Technical Studies (GITS).</p>
-            <div className='text-left pb-8 flex items-start pt-6' > 
-            <CustomButton /> 
-            </div>
-            {/* <div className={`hidden lg:block  ${isFixed && "lg:hidden"}`}>
-              <CustomButton />
-            </div> */}
-          </div>
-          <div className="md:w-1/2 sm:mb-8 lg:mb-8 md:mb-0 relative" >
-            <img
-              src="images/reserch.jpg"
-              alt="Students"
-              className="w-full h-auto rounded-2xl  shadow-lg"
-            />
+    <Container>
+      <p className="text-xl sm:text-2xl text-center text-primary mb-3 md:mb-8 mt-16">
+        Reserch at GITS
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-7 lg:grid-cols-9 md:place-items-center">
+        <div className="md:col-span-3 lg:col-span-5 md:pr-2">
+          <h3 className="my-4 md:my-2 lg:my-4 flex md:flex-col gap-2 lg:gap-4 text-2xl sm:text-4xl lg:text-5xl font-bold text-[#231f20] items-center justify-center md:items-start">
+            <span className="block">EXPLORING THE</span>
+            <span className="block">UNEXPLORED</span>
+          </h3>
+          <p className="text-sm text-justify">
+            Rapid growth in scientific knowledge is an indication of quest for
+            innovations which has an impact on economic and societal
+            development. Science, technology and innovation is often initiated
+            in the College environment and it is also the mission of Geetanjali
+            Institute Of Technical Studies (GITS).
+          </p>
+          <div className="text-left flex items-start py-6 md:py-3 lg:py-6">
+            <CustomButton />
           </div>
         </div>
-              
+        <div className="relative md:col-span-4">
+          <img
+            src="images/reserch.jpg"
+            alt="Students"
+            className="w-full h-auto rounded-2xl  shadow-lg"
+          />
+        </div>
       </div>
-      <div className="w-[60rem]  sm:h-1 lg:h-1 bg-primary mx-auto mb-6 lg:mb-10"></div>
-    </div>
-    
+      <div className="w-full h-1 bg-primary mt-6 mb-16" />
+    </Container>
   );
 };
 
 export default Reserch;
-
