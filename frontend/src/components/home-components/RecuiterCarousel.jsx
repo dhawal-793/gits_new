e53e0dad56617b58f65707821b0dc4c343ea2245
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-07 20:52:53 +0530

    Renamed Recruiter to RecruiterCarousel, updated structure and styles.

---
+++ frontend/src/components/home-components/RecuiterCarousel.jsx
@@ -0,0 +1,58 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+
+const recruiters = [
+  {
+    name: "Amazon",
+    logo: "images/recruiters/1.jpg",
+  },
+  {
+    name: "Flipkart",
+    logo: "images/recruiters/2.jpg",
+  },
+  {
+    name: "Microsoft",
+    logo: "images/recruiters/3.jpg",
+  },
+  {
+    name: "Infosys",
+    logo: "images/recruiters/4.jpg",
+  },
+  {
+    name: "TCS",
+    logo: "images/recruiters/5.jpg",
+  },
+];
+
+const RecruiterCarousel = () => {
+  return (
+    <div className="w-full h-auto mx-auto">
+      <Swiper
+        grabCursor={true}
+        spaceBetween={20}
+        centeredSlides={true}
+        slidesPerView={3}
+        autoplay={true}
+        loop={true}
+        pagination={true}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+        infinite={true}
+      >
+        {recruiters.map((recruiter, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={recruiter.logo}
+              alt={recruiter.name}
+              className="size-24 sm:size-32 lg:size-40 mx-auto object-contain"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default RecruiterCarousel;
