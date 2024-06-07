187dbfe7a03be6293d86c9b145ea67e77ee2a423
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-07 20:51:21 +0530

    Renamed section3 to WhyChooseGITS, updated structure and styles.

---
+++ frontend/src/components/home-components/WhyChooseGITS.jsx
@@ -0,0 +1,94 @@
+import { Link } from "react-router-dom";
+
+import Container from "../ui/Container";
+
+const WhyChooseGITS = () => {
+  const items = [
+    {
+      link: "/",
+      icon: "fa-solid fa-book",
+      title: "COURSES",
+    },
+    {
+      link: "/",
+      icon: "fa-solid fa-people-group",
+      title: "PLACEMENTS",
+    },
+    {
+      link: "/",
+      icon: "fa-solid fa-trophy",
+      title: "AWARDS",
+    },
+    {
+      link: "/",
+      icon: "fa-solid fa-newspaper",
+      title: "NEWSTRACK",
+    },
+    {
+      link: "/",
+      icon: "fa-regular fa-file",
+      title: "BROCHURE",
+    },
+    {
+      link: "/",
+      icon: "fa-solid fa-handshake-simple",
+      title: "MOU's",
+    },
+  ];
+
+  return (
+    <>
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="w-full pr-4 mb-6">
+            <p className="text-2xl font-bold mb-4">Why GITS?</p>
+            <h2 className="text-3xl font-bold mb-6 gap-2 flex flex-wrap">
+              <span>REDEFINING</span>
+              <span className="text-primary">ACADEMIC&nbsp;EXCELLENCE</span>
+            </h2>
+            <p className="mb-4 text-sm text-justify">
+              GITS has best in-class infrastructure, including the
+              state-of-the-art research facilities and a modern digital library.
+              As a multi-disciplinary College, GITS offers career-oriented
+              courses at UG, PG and Doctoral levels, across diverse streams.
+              Currently, more than 2000 students are enrolled across all
+              programs. A well-qualified team of 70 faculty members is the asset
+              of the University.
+            </p>
+            <Link
+              to="why-gits"
+              className="w-fit flex items-center gap-2 font-semibold text-white bg-secondary hover:scale-x-105 transition-all duration-200 px-4 py-2 md:px-6 md:py-4 rounded-md"
+            >
+              <span>Explore more</span>
+              <span className="fas fa-chevron-right" />
+            </Link>
+          </div>
+          <div className="w-full md:ml-4 py-6 px-2 rounded-xl bg-gradient-to-b from-primary/70 to-primary/20">
+            <div className="grid grid-cols-2 gap-4 lg:grid-cols-3">
+              {items.map((item, index) => (
+                <div key={index} className="w-full px-2 mb-4">
+                  <a
+                    href={item.link}
+                    className=" bg-yellow-100 hover:bg-primary/50 rounded-lg shadow-md text-center backdrop-blur-lg bg-opacity-50 grid grid-cols-1 gap-4 place-items-center py-4"
+                  >
+                    <div className="mx-auto ">
+                      <i
+                        className={`${item.icon} text-2xl sm:text-3xl lg:text-4xl`}
+                      ></i>
+                    </div>
+                    <h4 className="mt-2 text-sm sm:text-base xl:text-lg font-semibold tracking-wider">
+                      {item.title}
+                    </h4>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-1 bg-primary mt-6 mb-16" />
+      </Container>
+    </>
+  );
+};
+
+export default WhyChooseGITS;
