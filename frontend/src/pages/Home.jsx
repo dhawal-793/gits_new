74ae83cc62aa30d7ac93a8f742abf55de69efdde
Somya Panchal <somyapancom@gmail>
2024-06-08 23:09:56 +0530

    added component in home section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import WhatsDifferent from "../components/home-components/WhatsDifferent";
 import HomePage from "../components/home-components/Rcarosel";
 import Reserch from "../components/home-components/Reserch";
 import SectionPage from "../components/home-components/Section3";
-
+import PlacementInfo from "../components/home-components/Section4";
+import LearningComponent from "../components/home-components/Inovation";
 const Home = () => {
   return (
     <>
@@ -11,7 +12,9 @@ const Home = () => {
       <WhatsDifferent />
       <Reserch/>
       <SectionPage/>
+      <PlacementInfo/>
       <HomePage />
+      <LearningComponent/>
       
      
     </>
