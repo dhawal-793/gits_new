e4189755bee40713730979a4f501bc5266eb431b
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-09 15:57:04 +0530

    Updated FundedResearch Page to display data using api.

--- frontend/src/pages/FundedResearch.jsx
+++ frontend/src/pages/FundedResearch.jsx
@@ -1,12 +1,65 @@
-import Container from "../components/ui/Container";
+import { useEffect, useState } from "react";
+
+import { fetchMdx } from "../lib/fetch-mdx";
+
+import Title from "../components/ui/Title";
+import MarkdownContainer from "../components/MarkdownContainer";
+import NotFound from "../components/errors/NotFound";
+import InternalServerError from "../components/errors/InternalServerError";
 
 const FundedResearch = () => {
+  const [mdxContent, setMdxContent] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchMdx("rnd/funded-research");
+        console.log(data);
+        if (data == "404") {
+          setMdxContent("404");
+          setLoading(false);
+          return;
+        }
+        if (data == "500") {
+          setMdxContent("500");
+          setLoading(false);
+          return;
+        }
+        setMdxContent(data.content);
+      } catch (error) {
+        console.error("Error fetching MDX content:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchContent();
+  }, []);
+
+  if (mdxContent == "404") {
+    return <NotFound />;
+  }
+  if (mdxContent == "500") {
+    return <InternalServerError />;
+  }
+
   return (
-    <Container>
-      <div className="min-h-screen h-auto w-full flex items-center justify-center">
-        <p className="font-bold text-3xl uppercase">Funded Research</p>
-      </div>
-    </Container>
+    <>
+      <Title title="funded research" />
+      {loading ? (
+        <div className="min-h-[80vh] w-full flex items-center justify-center text-center">
+          <p>Loading...</p>
+        </div>
+      ) : mdxContent ? (
+        <>
+          <MarkdownContainer>{mdxContent}</MarkdownContainer>
+        </>
+      ) : (
+        <div className="min-h-[80vh] w-full" />
+      )}
+    </>
   );
 };
 
