a8100745f93cbeaa20fb8a1c7039f6a47d327889
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-09 15:51:49 +0530

    Created api route for fetching data on rnd route.

---
+++ backend/api/rnd.js
@@ -0,0 +1,71 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { fileURLToPath } from "url";
+
+const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rndDirectory = path.join(__dirname, "../data/rnd");
+
+if (!fs.existsSync(rndDirectory)) {
+  fs.mkdirSync(rndDirectory);
+}
+
+router.get("/:rnd", (req, res) => {
+  const { rnd } = req.params;
+  // console.log(admission);
+  const filePath = path.join(rndDirectory, `${rnd}.mdx`);
+  // console.log(filePath);
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      // console.log(err);
+      return res.status(404).send("File not found");
+    }
+    const { data: frontmatter, content } = matter(data);
+    res.send({
+      title: frontmatter.title,
+      content: content,
+    });
+  });
+});
+
+export default router;
+
+// router.get("/", (req, res) => {
+//   fs.readdir(adissionsDirectory, (err, files) => {
+//     if (err) {
+//       return res.status(500).json({ error: "Failed to load posts" });
+//     }
+//     const posts = files.map((file) => {
+//       const content = fs.readFileSync(path.join(adissionsDirectory, file), "utf-8");
+//       const [title, ...body] = content.split("\n");
+//       return {
+//         id: file.replace(".mdx", ""),
+//         title: title.replace("# ", ""),
+//         content: body.join("\n"),
+//       };
+//     });
+//     res.json(posts);
+//   });
+// });
+
+// router.post("/", (req, res) => {
+//   const { title, content } = req.body;
+//   const id = Date.now().toString();
+//   const markdownContent = `# ${title}\n\n${content}`;
+//   fs.writeFile(
+//     path.join(adissionsDirectory, `${id}.md`),
+//     markdownContent,
+//     (err) => {
+//       if (err) {
+//         return res.status(500).json({ error: "Failed to save post" });
+//       }
+//       res.json({ id, title, content });
+//     }
+//   );
+// });
+
+// export default router;
