523de9da0dbe6f9d8ee0ead180f7042d78e1a9be
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-09 15:50:10 +0530

    Updated the imports and added space in y-axis between all the sections.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,19 +3,20 @@ import WhatsDifferent from "../components/home-components/WhatsDifferent";
 import Reserch from "../components/home-components/Reserch";
 import WhyChooseGITS from "../components/home-components/WhyChooseGITS";
 import Recruiters from "../components/home-components/Recruiters";
-import LearningComponent from "../components/home-components/Inovation"
-import PlacementInfo from "../components/home-components/Section4"
+import LearningWithInnovation from "../components/home-components/LearningWithInnovation";
+import OurPlacements from "../components/home-components/OurPlacements";
 const Home = () => {
   return (
     <>
-      <MainCarousel />
-      <WhatsDifferent />
-      <Reserch />
-      <WhyChooseGITS />
-      <Recruiters />
-      <PlacementInfo/>
-      <LearningComponent/>
-      
+      <div className="space-y-8">
+        <MainCarousel />
+        <WhatsDifferent />
+        <Reserch />
+        <WhyChooseGITS />
+        <OurPlacements />
+        <LearningWithInnovation />
+        <Recruiters />
+      </div>
     </>
   );
 };
