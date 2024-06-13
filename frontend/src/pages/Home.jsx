7749923ba892cd1835ca1a64e66c87b235cc71e5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-13 09:25:27 +0530

    Wrapped different sections of Home Page in AnimatedWrapper.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,17 +5,32 @@ import WhyChooseGITS from "../components/home-components/WhyChooseGITS";
 import Recruiters from "../components/home-components/Recruiters";
 import LearningWithInnovation from "../components/home-components/LearningWithInnovation";
 import OurPlacements from "../components/home-components/OurPlacements";
+
+import AnimatedWrapper from "../components/ui/AnimatedWrapper";
+
 const Home = () => {
   return (
     <>
       <div className="space-y-8 w-full">
         <MainCarousel />
-        <WhatsDifferent />
-        <Reserch />
-        <WhyChooseGITS />
-        <OurPlacements />
-        <LearningWithInnovation />
-        <Recruiters />
+        <AnimatedWrapper animateFrom="bottom" delay={0.2}>
+          <WhatsDifferent />
+        </AnimatedWrapper>
+        <AnimatedWrapper animateFrom="right">
+          <Reserch />
+        </AnimatedWrapper>
+        <AnimatedWrapper animateFrom="left">
+          <WhyChooseGITS />
+        </AnimatedWrapper>
+        <AnimatedWrapper animateFrom="right">
+          <OurPlacements />
+        </AnimatedWrapper>
+        <AnimatedWrapper>
+          <LearningWithInnovation />
+        </AnimatedWrapper>
+        <AnimatedWrapper>
+          <Recruiters />
+        </AnimatedWrapper>
       </div>
     </>
   );
