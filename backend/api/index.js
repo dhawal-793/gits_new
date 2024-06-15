0ee127d3565225601debe21d37e646dfc33fcb52
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-15 13:12:16 +0530

    Added api route inside index.js and api/index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,6 +7,7 @@ import managementsMessageRoute from "./management-teams-message.js";
 import partnerRoute from "./partners.js";
 import admissionRoute from "./admissions.js";
 import rndRoute from "./rnd.js";
+import blogsRoute from "./blogs.js";
 
 config();
 // connectToDB()
@@ -25,10 +26,9 @@ app.use("/api/managements-message", managementsMessageRoute);
 app.use("/api/partners", partnerRoute);
 app.use("/api/admissions", admissionRoute);
 app.use("/api/rnd", rndRoute);
+app.use("/api/blogs", blogsRoute);
 
 app.get("/", (req, res) => res.redirect(process.env.FRONTEND));
-
-
 
 // app.listen(5000, (req, res) => {
 //   console.log("api server running");
--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import managementsMessageRoute from "./api/management-teams-message.js";
 import partnerRoute from "./api/partners.js";
 import admissionRoute from "./api/admissions.js";
 import rndRoute from "./api/rnd.js";
+import blogsRoute from "./api/blogs.js";
 
 // import connectToDB from './db.js'
 // connectToDB()
@@ -26,6 +27,7 @@ app.use("/api/managements-message", managementsMessageRoute);
 app.use("/api/partners", partnerRoute);
 app.use("/api/admissions", admissionRoute);
 app.use("/api/rnd", rndRoute);
+app.use("/api/blogs", blogsRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({ data: "data from server" });
