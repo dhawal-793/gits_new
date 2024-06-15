867f61320ad28be4c5a1e31f280a9b384ec07d29
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-15 19:06:48 +0530

    Added animated wrapper for  recruiter carousel section.

--- frontend/src/components/home-components/Recruiters.jsx
+++ frontend/src/components/home-components/Recruiters.jsx
@@ -1,3 +1,4 @@
+import AnimatedWrapper from "../ui/AnimatedWrapper";
 import Container from "../ui/Container";
 
 import RecruiterCarousel from "./RecuiterCarousel";
@@ -6,7 +7,7 @@ const Recruiters = () => {
   return (
     <Container>
       <div className="flex flex-col md:flex-row items-center gap-6">
-        <div className="w-full lg:w-1/2 mb-8 hidden lg:block">
+        <div className="w-full lg:w-1/2 mb-8 hidden lg:block z-10">
           <img
             src="images/recruiter-logo.png"
             alt="Students"
@@ -14,14 +15,14 @@ const Recruiters = () => {
           />
         </div>
         <div className="w-full lg:w-1/2 h-auto text-center lg:text-left">
-          <p className="text-lg sm:text-xl md:text-2xl text-primary mb-2">
-            Top Recruiters
-          </p>
-          <h3 className="my-4 flex  lg:flex-col gap-2 lg:gap-4 text-xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-[#231f20] items-center justify-center lg:items-start">
-            <span className="block">FORMULATE</span>
-            <span className="block">YOUR FUTURE!</span>
-          </h3>
-          <RecruiterCarousel />
+          <div className="w-full hidden lg:block">
+            <AnimatedWrapper animateFrom="left">
+              <Content />
+            </AnimatedWrapper>
+          </div>
+          <div className="lg:hidden">
+            <Content />
+          </div>
         </div>
       </div>
       <div className="w-full h-1 bg-primary my-6" />
@@ -30,3 +31,18 @@ const Recruiters = () => {
 };
 
 export default Recruiters;
+
+const Content = () => {
+  return (
+    <>
+      <p className="text-lg sm:text-xl md:text-2xl text-primary mb-2">
+        Top Recruiters
+      </p>
+      <h3 className="my-4 flex  lg:flex-col gap-2 lg:gap-4 text-xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-[#231f20] items-center justify-center lg:items-start">
+        <span className="block">FORMULATE</span>
+        <span className="block">YOUR FUTURE!</span>
+      </h3>
+      <RecruiterCarousel />
+    </>
+  );
+};
