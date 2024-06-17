5c9d20a445ac869d3a3255d73fd06629fb0f2af5
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-17 10:45:58 +0530

    Wrapped new components in AnimatedWrapper.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import WhyChooseGITS from "../components/home-components/WhyChooseGITS";
 import Recruiters from "../components/home-components/Recruiters";
 import LearningWithInnovation from "../components/home-components/LearningWithInnovation";
 import OurPlacements from "../components/home-components/OurPlacements";
-import CorporateTour from "../components/home-components/CorporateTour"
+import CorporateTour from "../components/home-components/CorporateTour";
 import AnimatedWrapper from "../components/ui/AnimatedWrapper";
 import Facilities from "../components/home-components/Facilities";
 const Home = () => {
@@ -13,27 +13,30 @@ const Home = () => {
     <>
       <div className="space-y-8 w-full">
         <MainCarousel />
-        <AnimatedWrapper animateFrom="bottom" delay={0.4}>
+        <AnimatedWrapper animateFrom="bottom" delay={0.3}>
           <WhatsDifferent />
         </AnimatedWrapper>
         <AnimatedWrapper animateFrom="right">
           <Reserch />
         </AnimatedWrapper>
         <AnimatedWrapper animateFrom="left">
+          <Facilities />
+        </AnimatedWrapper>
+        <AnimatedWrapper animateFrom="bottom">
           <WhyChooseGITS />
         </AnimatedWrapper>
-        <Facilities/>
         <AnimatedWrapper animateFrom="right">
           <OurPlacements />
         </AnimatedWrapper>
         <AnimatedWrapper>
           <LearningWithInnovation />
         </AnimatedWrapper>
-        <CorporateTour/>
+        <AnimatedWrapper>
+          <CorporateTour />
+        </AnimatedWrapper>
         <AnimatedWrapper>
           <Recruiters />
         </AnimatedWrapper>
-        
       </div>
     </>
   );
