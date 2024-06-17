e2dfc45f723fa6806e1bb223581ace592fb721f2
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-06-17 10:44:40 +0530

    Updated structure and styles.

--- frontend/src/components/home-components/LearningWithInnovation.jsx
+++ frontend/src/components/home-components/LearningWithInnovation.jsx
@@ -1,13 +1,12 @@
 import Container from "../ui/Container";
-import Innovation from "./InnovationCarousel"
+import InnovationCarousel from "./InnovationCarousel";
 
 const LearningWithInnovation = () => {
   return (
     <Container>
       <div className="text-center relative ">
-        <h2 className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl font-extrabold uppercase tracking-wider opacity-50 innovations-text">
+        <h2 className="text-[2.8rem] sm:text-6xl md:text-8xl lg:text-9xl font-extrabold uppercase tracking-wider opacity-50 innovations-text">
           Innovations
-          
         </h2>
         <h3 className="absolute inset-x-0 top-1/2 sm:text-2xl text-sm md:text-3xl font-semibold text-black/80 -translate-y-1/2">
           With Us There Are So Many Ways of Learning
@@ -16,8 +15,8 @@ const LearningWithInnovation = () => {
       <h3 className="text-xl  font-semibold sm:text-4xl md:text-4xl text-black text-center my-3">
         We Make Engineering Fun
       </h3>
-      <div className="flex justify-center gap-6">
-      <Innovation/>
+      <div className="">
+        <InnovationCarousel />
       </div>
       <div className="w-full h-1 bg-primary my-6" />
     </Container>
