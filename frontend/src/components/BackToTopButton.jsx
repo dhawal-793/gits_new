858b6983c4e31d20a6e9323103e95b331d29ad14
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-07-08 20:26:09 +0530

    Fixed margin for container, updated BackToTopButton styles.

--- frontend/src/components/BackToTopButton.jsx
+++ frontend/src/components/BackToTopButton.jsx
@@ -16,14 +16,13 @@ const BackToTopButton = () => {
   }, []);
 
   return (
-    <button onClick={handleScroll} className="">
-      <div
-        className={`transition-all ${
-          btnVisiblity ? "translate-y-0" : "-translate-y-[100vh]"
-        } z-40 fixed bottom-2 right-2 xs:bottom-[1.3rem] xs:right-[1.3rem] md:bottom-4 md:right-4 w-8 h-8 flex justify-center items-center rounded-full bg-secondary cursor-pointer  hover:scale-[1.08] duration-500 text-white`}
-      >
-        <span className="fa-solid fa-chevron-up" />
-      </div>
+    <button
+      onClick={handleScroll}
+      className={`transition-all ${
+        btnVisiblity ? "translate-y-0" : "-translate-y-[100vh]"
+      } z-40 fixed bottom-2 right-2 xs:bottom-[1.3rem] xs:right-[1.3rem] md:bottom-4 md:right-4 w-8 h-8 flex justify-center items-center rounded-full bg-secondary cursor-pointer  hover:scale-[1.08] duration-500 text-white`}
+    >
+      <span className="fa-solid fa-chevron-up" />
     </button>
   );
 };
